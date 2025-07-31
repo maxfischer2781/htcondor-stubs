d9a06959c61e15b252c3543ee398aac8029ef6c1
Max Kühn <maxfischer2781@gmail.com>
2025-07-31 19:05:56 +0200

    partial Schedd stubs

--- htcondor-stubs/__init__.pyi
+++ htcondor-stubs/__init__.pyi
@@ -1,7 +1,8 @@
-from typing import Any, ClassVar
+from typing import Any, ClassVar, Callable, Literal, SupportsInt
+from warnings import deprecated
 import enum
 
-from classad import ClassAd
+from classad import ClassAd, ExprTree
 
 class DaemonTypes(enum.IntEnum):
     Any = 1
@@ -61,3 +62,73 @@ class Collector:
         projection: list[str] = [],
         statistics: str = "",
     ) -> ClassAd[Any]: ...
+
+# Schedd
+
+class QueryOpts(enum.IntEnum):
+    Default = 0
+    AutoCluster = 1
+    GroupBy = 2
+    DefaultMyJobsOnly = 4
+    SummaryOnly = 8
+    IncludeClusterAd = 16
+    IncludeJobsetAds = 32
+    ClusterAds = 80
+    JobsetAds = 96
+    names: ClassVar[dict[str, QueryOpts]]
+    values: ClassVar[dict[int, QueryOpts]]
+
+class JobAction(enum.IntEnum):
+    Hold = 1
+    Release = 2
+    Remove = 3
+    RemoveX = 4
+    Vacate = 5
+    VacateFast = 6
+    Suspend = 8
+    Continue = 9
+    names: ClassVar[dict[str, JobAction]]
+    values: ClassVar[dict[int, JobAction]]
+
+class TransactionFlags(enum.IntEnum):
+    NonDurable = 1
+    SetDirty = 4
+    ShouldLog = 8
+    names: ClassVar[dict[str, TransactionFlags]]
+    values: ClassVar[dict[int, TransactionFlags]]
+
+class Schedd:
+    def __init__(self, location_ad: ClassAd[Any] | None) -> None: ...
+    @deprecated("use .submit() instead")
+    def transaction(self, flags: int = 0, continue_txn: bool = False) -> Any: ...
+    def query(
+        self,
+        constraint: str = "true",
+        projection: list[str] = [],
+        callback: Callable[[ClassAd[Any]], None] | None = None,
+        limit: int = -1,
+        opts: QueryOpts = QueryOpts.Default,
+    ) -> list[ClassAd[Any]]: ...
+    @deprecated("use .query() instead")
+    def xquery(
+        self,
+        constraint: str = "true",
+        projection: list[str] = [],
+        callback: Callable[[ClassAd[Any]], None] | None = None,
+        limit: int = -1,
+        opts: QueryOpts = QueryOpts.Default,
+        name: str | None = None,
+    ) -> Any: ...
+    def act(
+        self,
+        action: JobAction,
+        job_spec: str | list[str] | ExprTree[Any],
+        reason: str | None = None,
+    ) -> ClassAd[Any]: ...
+    def edit(
+        self,
+        job_spec: str | list[str],
+        attr: str,
+        value: str | ExprTree[Any],
+        flags: TransactionFlags | Literal[0] = 0,
+    ) -> SupportsInt: ...
