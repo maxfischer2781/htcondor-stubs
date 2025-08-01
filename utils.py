f5d5a88d1636f7f4a61c25000f374ac8a38f1cd2
Max Kühn <maxfischer2781@gmail.com>
2025-08-02 00:16:05 +0200

    utility to format exceptions

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import Protocol, ClassVar
+from typing import Any, Protocol, ClassVar
 
 
 class HTCondorEnum(Protocol):
@@ -14,3 +14,18 @@ def format_enum(enum: "type[HTCondorEnum]") -> str:
     body.append(f"    names: ClassVar[dict[str, {enum.__qualname__}]]")
     body.append(f"    values: ClassVar[dict[int, {enum.__qualname__}]]")
     return "\n".join(body)
+
+
+def format_exception(exception: BaseException) -> str:
+    """Format an HTCondor Exception class for type hinting"""
+    bases = ",".join(base.__qualname__ for base in exception.__bases__)
+    return f"class {exception.__qualname__}({bases}): ..."
+
+
+def get_exceptions(module: "Any") -> "list[type[BaseException]]":
+    """Get all exceptions of a module"""
+    return [
+        c
+        for c in vars(module).values()
+        if isinstance(c, type) and issubclass(c, BaseException)
+    ]
