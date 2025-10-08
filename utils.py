7080ed51904907d39baabbdfab22b66ef929e44f
Max Kühn <maxfischer2781@gmail.com>
2025-10-08 14:34:06 +0200

    add helper for new style enums

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from typing import Protocol, ClassVar
 from types import ModuleType
+import enum
 
 
 class HTCondorEnum(Protocol):
@@ -8,13 +9,13 @@ class HTCondorEnum(Protocol):
     values: "ClassVar[dict[int, HTCondorEnum]]"
 
 
-def format_enum(enum: "type[HTCondorEnum]") -> str:
+def format_htc_enum(h_enum: "type[HTCondorEnum]") -> str:
     """Format an HTCondor Enum class for type hinting"""
-    body = [f"class {enum.__qualname__}(enum.IntEnum):"]
-    for value, obj in enum.values.items():
+    body = [f"class {h_enum.__qualname__}(enum.IntEnum):"]
+    for value, obj in h_enum.values.items():
         body.append(f"    {obj.name} = {value}")
-    body.append(f"    names: ClassVar[dict[str, {enum.__qualname__}]]")
-    body.append(f"    values: ClassVar[dict[int, {enum.__qualname__}]]")
+    body.append(f"    names: ClassVar[dict[str, {h_enum.__qualname__}]]")
+    body.append(f"    values: ClassVar[dict[int, {h_enum.__qualname__}]]")
     return "\n".join(body)
 
 
@@ -22,6 +23,23 @@ def format_exception(exception: "type[BaseException]") -> str:
     """Format an HTCondor Exception class for type hinting"""
     bases = ",".join(base.__qualname__ for base in exception.__bases__)
     return f"class {exception.__qualname__}({bases}): ..."
+
+
+def format_enum(p_enum: "type[enum.Enum]") -> str:
+    """Format a Python Enum class for type hinting"""
+    body = [
+        f"class {p_enum.__qualname__}("
+        + ",".join(
+            f"{base.__module__}.{base.__qualname__}" for base in p_enum.__bases__
+        )
+        + "):"
+    ]
+    # use `dict` to ensure entries are unique
+    for member in dict.fromkeys(
+        sorted(p_enum.__members__.values(), key=lambda item: item.value)
+    ):
+        body.append(f"    {member.name} = {member.value}")
+    return "\n".join(body)
 
 
 def get_exceptions(module: "ModuleType") -> "list[type[BaseException]]":
